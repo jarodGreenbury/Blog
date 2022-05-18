f2d5abe41fd0a51d00f1665c774502bdea510677
jarodGreenbury <greenbury.jarod@gmail.com>
2022-05-18 17:08:45 +1200

    Props and List

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,17 @@
+import { useState } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
-    
-    const handleClick = () =>{
-        console.log('hello');
-    }
+    const [blogs, setBlogs] = useState([
+        {title: 'My new website', body: 'hello this is my blog', author: 'Jarod', id: 1},
+        {title: 'My  website', body: 'my blog', author: 'bob', id: 2},
+        {title: 'website', body: 'hello ', author: 'rod', id: 3}
+    ]);
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
-            <button onClick={handleClick}>Click me</button>
+            <BlogList blogs = {blogs}/>
         </div>
      );
 }
