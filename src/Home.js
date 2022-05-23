fe6d53779b8f3ec367c5bbcf7030eb0b1ddf8101
jarodGreenbury <greenbury.jarod@gmail.com>
2022-05-23 19:15:16 +1200

    props

--- src/Home.js
+++ src/Home.js
@@ -10,8 +10,8 @@ const Home = () => {
 
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <BlogList blogs = {blogs}/>
+            
+            <BlogList blogs = {blogs} title = "All blogs"/>
         </div>
      );
 }
