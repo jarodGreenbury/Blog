60e3c773db848ffbc2c1ac264634eef510a6edaf
jarodGreenbury <greenbury.jarod@gmail.com>
2022-05-23 19:26:47 +1200

    Props as Functions
    creating a delete blog prop/function

--- src/Home.js
+++ src/Home.js
@@ -8,10 +8,15 @@ const Home = () => {
         {title: 'website', body: 'hello ', author: 'rod', id: 3}
     ]);
 
+    const handleDelete = (id) =>{
+        const newBlogs = blogs.filter(blog => blog.id !== id)
+        setBlogs(newBlogs)
+    }
+
     return ( 
         <div className="home">
             
-            <BlogList blogs = {blogs} title = "All blogs"/>
+            <BlogList blogs = {blogs} title = "All blogs" handleDelete={handleDelete}/>
         </div>
      );
 }
